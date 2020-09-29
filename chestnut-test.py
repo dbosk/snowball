daec24a17ade89fb17456fd0c051f90ee2c0e08a
Nils Jörgensen <nilsjor@UG.KTH.SE>
2020-09-29 16:38:53 +0200

    Filter which item types get processed by the script, handle more exceptions

--- chestnut-test.py
+++ chestnut-test.py
@@ -17,13 +17,30 @@ sch = SemanticScholar(config['s2_api_url'], config['s2_api_key'], timeout=60)
 # Load 'base' collection, declare arrays for storing results
 base_papers = zot.collection_items(config['zot_base_collection'])
 references = []
+failed_base_papers = []
 orphaned_items = {}
 
-# For each paper in the base collection, get all works cited from S2
+# Retrieve all items in the base collection, and check their type
 for base_paper in base_papers:
-	base_paper_s2 = sch.paper(base_paper['data']['DOI'])
-	print('{}:'.format(base_paper['data']['title']))
+	item_type = base_paper['data']['itemType']
 
+	if item_type == 'journalArticle' or item_type == 'conferencePaper':
+		base_paper_doi = base_paper['data']['DOI'];
+	elif item_type == 'document':
+		base_paper_doi = base_paper['data']['url'].strip('https://doi.org/');
+	# Skip the item if it is none of the accepted types
+	else: continue
+
+	# For a valid item in the base collection, try to retrieve it from S2
+	try: base_paper_s2 = sch.paper(base_paper_doi)
+	except:
+		failed_base_papers.append(base_paper)
+		continue
+
+	print('{}:'.format(base_paper['data']['title']))
+	references.clear()
+
+	# Get references from S2
 	for reference in base_paper_s2['references']:
 
 		# If the DOI field is missing, skip this entry
@@ -51,18 +68,22 @@ for base_paper in base_papers:
 		# Add the entry to the references array
 		references.append(template)
 
-		# Create items in Zotero, and assign to the references collection
-		response = zot.create_items([template])
-		if(response['failed']):
-			print('WARNING: The following items were not created:')
-			pprint(response['failed'])
+	# Create items in Zotero, and assign to the references collection
+	response = zot.create_items(references)
+	if response['failed']:
+		print('WARNING: The following references were not created in Zotero:')
+		pprint(response['failed'])
 
-		for created in response['successful']:
-			for attempt in range(3):
-				added = zot.addto_collection(config['zot_ref_collection'], response['successful'][created])
-				if added: break
-			if not added: orphaned_items[created] = response['success'][created]
+	for created in response['successful']:
+		for attempt in range(3):
+			added = zot.addto_collection(config['zot_ref_collection'], response['successful'][created])
+			if added: break
+		if not added: orphaned_items[created] = response['success'][created]
 
-if orphaned_items:
-	print('WARNING: The following items were not added to the collection:')
+	if orphaned_items:
+		print('WARNING: The following references were not added to the collection:')
+		pprint(orphaned_items)
+
+if failed_base_papers:
+	print('WARNING: The following base papers failed to be retreived:')
 	pprint(orphaned_items)
