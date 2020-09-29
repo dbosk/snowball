cdf2ac9e685a89a252759f392208f57f2ded9a7f
Nils Jörgensen <nilsjor@UG.KTH.SE>
2020-09-29 15:36:27 +0200

    Handle case where Zotero items are not created/added to collection

--- chestnut-test.py
+++ chestnut-test.py
@@ -1,3 +1,4 @@
+from pprint import pprint
 import yaml
 from pyzotero import zotero
 from semanticscholar import SemanticScholar
@@ -13,21 +14,22 @@ zot = zotero.Zotero(config['zot_library_id'], config['zot_library_type'], config
 # Configure Semantic Scholar client
 sch = SemanticScholar(config['s2_api_url'], config['s2_api_key'], timeout=60)
 
-# Load 'base' collection, declare array for storing references
+# Load 'base' collection, declare arrays for storing results
 base_papers = zot.collection_items(config['zot_base_collection'])
 references = []
+orphaned_items = {}
 
 # For each paper in the base collection, get all works cited from S2
 for base_paper in base_papers:
 	base_paper_s2 = sch.paper(base_paper['data']['DOI'])
+	print('{}:'.format(base_paper['data']['title']))
 
 	for reference in base_paper_s2['references']:
 
 		# If the DOI field is missing, skip this entry
 		if not reference['doi']: continue
-		pprint(reference['doi'])
-
 		paper = sch.paper(reference['doi'])
+		print(' ''{}'' -> {}'.format(len(references), paper['title']))
 
 		# Use the Zotero template for Journal Articles for all publications
 		template = zot.item_template('journalArticle')
@@ -49,9 +51,18 @@ for base_paper in base_papers:
 		# Add the entry to the references array
 		references.append(template)
 
-# When all works cited, for each publication in the base collection has been added,
-#  to the array, create items in Zotero, and assign to the references bollection
-resp = zot.create_items(references)
-if(resp['failed']):	print(resp['failed'])
-for success in resp['successful']:
-	zot.addto_collection(config['zot_ref_collection'], resp['successful'][success])
+		# Create items in Zotero, and assign to the references collection
+		response = zot.create_items([template])
+		if(response['failed']):
+			print('WARNING: The following items were not created:')
+			pprint(response['failed'])
+
+		for created in response['successful']:
+			for attempt in range(3):
+				added = zot.addto_collection(config['zot_ref_collection'], response['successful'][created])
+				if added: break
+			if not added: orphaned_items[created] = response['success'][created]
+
+if orphaned_items:
+	print('WARNING: The following items were not added to the collection:')
+	pprint(orphaned_items)
