156124d01d881a63cfdb894f24e0e291dbd6efe1
Nils Jörgensen <nilsjor@UG.KTH.SE>
2020-09-29 12:51:03 +0200

    Change to S2 Partners API
    Private key is read from a YAML file

--- chestnut-test.py
+++ chestnut-test.py
@@ -1,32 +1,36 @@
 import sys
+import yaml
 from pyzotero import zotero
 sys.path.append("/mnt/c/git/semanticscholar")
 import semanticscholar as sch
 from pprint import pprint
 from nameparser import HumanName
 
-library_id = 2569621
-library_type = 'group'
-api_key = 'cAjS7ISgL7i6f0XesVCS4Hfq'
+# Load external config params
+config = yaml.safe_load(open('config.yaml'))
 
-zot = zotero.Zotero(library_id, library_type, api_key)
+# Configure Zotero client
+zot = zotero.Zotero(config['zot_library_id'], config['zot_library_type'], config['zot_api_key'])
 
-base_papers = zot.collection_items('D7CPU28U')
-
+# Load 'base' collection, declare array for storing references
+base_papers = zot.collection_items(config['zot_base_collection'])
 references = []
 
+# For each paper in the base collection, get all works cited from S2
 for base_paper in base_papers:
-	base_paper_s2 = sch.paper(base_paper['data']['DOI'], timeout=60)
-	pprint(base_paper_s2)
+	base_paper_s2 = sch.paper(base_paper['data']['DOI'], timeout=60,
+		headers={'x-api-key': config['s2_api_key']})
 
 	for reference in base_paper_s2['references']:
 
+		# If the DOI field is missing, skip this entry
 		if not reference['doi']: continue
 		pprint(reference['doi'])
 
-		paper = sch.paper(reference['doi'], timeout=60)
-		paper.keys()
+		paper = sch.paper(reference['doi'], timeout=60,
+			headers={'x-api-key': config['s2_api_key']})
 
+		# Use the Zotero template for Journal Articles for all publications
 		template = zot.item_template('journalArticle')
 		template['title'] = paper['title']
 		template['DOI'] = paper['doi']
@@ -36,14 +40,19 @@ for base_paper in base_papers:
 		template['publicationTitle'] = paper['venue']
 		template['date'] = paper['year']
 
+		# Try to parse the authors' names using the HumanName package
 		template['creators'].clear()
 		for author in paper['authors']:
 			name = HumanName(author['name'])
-			template['creators'].append({'creatorType':'author','firstName':name.first,'lastName':name.last})
+			template['creators'].append({'creatorType':'author',
+				'firstName':name.first,'lastName':name.last})
 
+		# Add the entry to the references array
 		references.append(template)
 
+# When all works cited, for each publication in the base collection has been added,
+#  to the array, create items in Zotero, and assign to the references bollection
 resp = zot.create_items(references)
 if(resp['failed']):	print(resp['failed'])
 for success in resp['successful']:
-	zot.addto_collection('HXCTMQ9M', resp['successful'][success])
+	zot.addto_collection(config['zot_ref_collection'], resp['successful'][success])
